431dd370f380411164a33728e973a62a03810e7f
Ksondzyk <roman.kson@gmail.com>
2020-03-18 20:20:07 +0200

    new new

--- javaScript/lesson3/tast1/index.js
+++ javaScript/lesson3/tast1/index.js
@@ -40,7 +40,7 @@ console.log("'24' / 4: ", "24" / 4);
 18 * "3";
 console.log("18 * '3': ", 18 * "3");
 "9" - "3";
-console.log("'9' - '3: ", "9" - "3");
+console.log(`"9" - "3": `, "9" - "3");
 "seventeen" / 3;
 console.log("'seventeen' / 3: ", "seventeen" / 3);
 4 * "four";
@@ -49,7 +49,7 @@ console.log("4 * 'four': ", 4 * "four");
 console.log("17 / 0: ", 17 / 0);
 
 10 + "seven";
-console.log("10 + 'seven': ", 10 + "seven");
+console.log('10 + "seven": ', 10 + "seven");
 undefined + 1;
 console.log("undefined + 1: ", undefined + 1);
 null + 8;
