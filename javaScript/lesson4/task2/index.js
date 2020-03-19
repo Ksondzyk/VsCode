3a75703abe317462f85438e39535bec13fc1817e
Ksondzyk <roman.kson@gmail.com>
2020-03-19 20:01:54 +0200

    result

--- javaScript/lesson4/task2/index.js
+++ javaScript/lesson4/task2/index.js
@@ -1,13 +1,33 @@
-let m = 10;
-let n = 20;
+// let m = 10;
+// let n = 20;
+// let result = 1;
+
+// do {
+//   if (m % 2 === 1) {
+//     result *= m;
+//   }
+//   m++;
+// } while (m <= n);
+
+// console.log("Result: ", result);
+
+
+let m = 3;
+let n = 9;
+const m = 3;
+const n = 9;
 let result = 1;
 
 do {
   if (m % 2 === 1) {
     result *= m;
+for (let i = m; i <= n; i++) {
+  if (i % 2 === 1) {
+    result *= i;
   }
   m++;
 } while (m <= n);
 
+}
 console.log("Result: ", result);
 
