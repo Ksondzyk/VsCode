8ca1477d3fe47a2b5d7aed399fdb951c6d8d032b
Ksondzyk <roman.kson@gmail.com>
2020-03-19 20:15:07 +0200

    result

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -1,17 +1,15 @@
 let m = 10;
 let n = 12;
-let result = 0;
-let res = 0;
+let sum = 0;
 for (let i = m; i < n && n <= 50; i++) {
-  result += res;
   if (i % 2 === 0 || !(i % 4 === 0)) {
-    res += i + m + n;
+    sum += i + m + n;
   } else if (i % 3 === 0) {
-    res -= i;
+    sum -= i;
   } else if (i % 4 === 0) {
-    res *= i;
-  }
-  if (i % 5 === 0) {
+    sum *= i;
+  } else if (i % 5 === 0) {
     console.log(i);
   }
 }
+let result = sum;
