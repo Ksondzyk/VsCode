3df004adbb27a6747edf5433e4c6a2869986ee37
Ksondzyk <roman.kson@gmail.com>
2020-03-19 22:28:48 +0200

    Upload

--- javaScript/lesson4/hw5/index.js
+++ javaScript/lesson4/hw5/index.js
@@ -1,12 +1,10 @@
 let sum = 0;
-let res2;
-let res;
-let res1;
+
 for (let i = 0; i < 1000; i++) {
   sum += i;
-  res2 = sum % 1234;
-  res1 = sum / 1234;
-  res = res1 < res2;
 }
 
+let res2 = sum % 1234;
+let res1 = sum / 1234;
+let res = res1 < res2;
 console.log(!!res);
