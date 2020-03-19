012619d9ba17d3f4d910bee0b4d750e785033953
Ksondzyk <roman.kson@gmail.com>
2020-03-19 17:01:25 +0200

    reload

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -2,7 +2,7 @@ let m = 5;
 let n = 12;
 let result = 0;
 for (let i = m; i < n; i++) {
-  if (i === 2 && i != 4) {
+  if (i === 2 || i != 4) {
     result += i;
   } else if (i === 3) {
     result -= i;
@@ -11,6 +11,5 @@ for (let i = m; i < n; i++) {
   } else if (i === 5) {
     console.log(i);
   } else {
-    break;
   }
 }
