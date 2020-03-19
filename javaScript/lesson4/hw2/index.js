80fafd50d1f8b4d92e09a785d25c2815b10ef20a
Ksondzyk <roman.kson@gmail.com>
2020-03-19 20:39:04 +0200

    push123

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -5,9 +5,11 @@ let result = 0;
 for (let i = m; i < n && n <= 50; i++) {
   if (i % 2 === 0 || !(i % 4 === 0)) {
     sum += i + m + n;
-  } else if (i % 3 === 0) {
+  }
+  if (i % 3 === 0) {
     sum -= i;
-  } else if (i % 4 === 0) {
+  }
+  if (i % 4 === 0) {
     sum *= i;
   }
   result += sum;
