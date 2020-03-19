0e845b6db876f2543ec1aa5073d5eb4dd7832a49
Ksondzyk <roman.kson@gmail.com>
2020-03-19 18:27:44 +0200

    updead

--- javaScript/lesson4/hw3/index.js
+++ javaScript/lesson4/hw3/index.js
@@ -3,12 +3,13 @@ let sum = 0;
 for (let i = 0; i <= 1000; i++) {
   if (i % 2 === 1) {
     console.log("Found");
-    sum = i;
-    if (sum * 5 > 5000) {
+    sum += i;
+    let res = sum * 5;
+    if (res > 5000) {
       console.log("Bigger");
     } else {
       console.log("Smaller or equal");
     }
   }
-  console.log(sum);
+  //   console.log(sum);
 }
