c32e1064234d14eaad483e93399679975a2349d7
Ksondzyk <roman.kson@gmail.com>
2020-03-19 21:57:48 +0200

    commit

--- javaScript/lesson4/task2/index.js
+++ javaScript/lesson4/task2/index.js
@@ -1,25 +1,11 @@
-// let m = 10;
-// let n = 20;
-// let result;
-// let sum = 1;
 
-// do {
-//   if (m % 2 === 1) {
-//     sum *= m;
-//   }
-//   m++;
-// } while (m <= n);
-
-// result += sum;
-
-// console.log("Result: ", result);
-
-let m = 10;
-let n = 20;
+const m = 10;
+const n = 20;
 let result = 1;
 
-for (let m = 10; m <= n; m++) {
-  if (m % 2 === 1) continue;
-  result *= m;
+for (let i = m; i <= n; i++) {
+  if (i % 2 === 1) {
+    result *= i;
+  }
 }
 console.log("Result: ", result);
