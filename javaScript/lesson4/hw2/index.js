e546b3849391b14db346403fbd07e94be95581de
Ksondzyk <roman.kson@gmail.com>
2020-03-19 20:25:49 +0200

    var

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -1,6 +1,6 @@
 let m = 10;
 let n = 12;
-result = 0;
+var result = 0;
 for (let i = m; i < n && n <= 50; i++) {
   if (i % 2 === 0 || !(i % 4 === 0)) {
     result += i + m + n;
@@ -8,7 +8,8 @@ for (let i = m; i < n && n <= 50; i++) {
     result -= i;
   } else if (i % 4 === 0) {
     result *= i;
-  } else if (i % 5 === 0) {
+  }
+  if (i % 5 === 0) {
     console.log(i);
   }
 }
