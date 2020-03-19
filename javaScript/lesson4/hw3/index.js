9be19076a15739669d49fb68844b55c649387c70
Ksondzyk <roman.kson@gmail.com>
2020-03-19 18:35:17 +0200

    push 123

--- javaScript/lesson4/hw3/index.js
+++ javaScript/lesson4/hw3/index.js
@@ -1,12 +1,12 @@
-let sum = 0;
-
 for (let i = 0; i <= 1000; i++) {
+  let sum = 0;
   if (i % 2 === 1) {
     console.log("Found");
     sum += i;
     let res = sum * 5;
     if (res * 5 > 5000) {
       console.log("Bigger");
+      break;
     } else {
       console.log("Smaller or equal");
     }
