091217e32c314e276b7d6294347378e04ca40883
Ksondzyk <roman.kson@gmail.com>
2020-03-19 23:24:17 +0200

    Reload

--- javaScript/lesson5/hw1/index.js
+++ javaScript/lesson5/hw1/index.js
@@ -1,7 +1,7 @@
 function getSum(arg1, arg2) {
   let result;
-  if (arg1 % 2 === 0 && arg1 % 2 === 0) {
-    result = arg1 + arg1;
+  if (arg1 % 2 === 0 && arg2 % 2 === 0) {
+    result = arg1 + arg2;
     return result;
   }
 }
