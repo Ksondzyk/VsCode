1c0d60e0fe87ff05ad25e22bcc876bbd3fddd218
Ksondzyk <roman.kson@gmail.com>
2020-03-19 18:29:48 +0200

    reload

--- javaScript/lesson4/hw3/index.js
+++ javaScript/lesson4/hw3/index.js
@@ -5,7 +5,7 @@ for (let i = 0; i <= 1000; i++) {
     console.log("Found");
     sum += i;
     let res = sum * 5;
-    if (res > 5000) {
+    if (res * 5 > 5000) {
       console.log("Bigger");
     } else {
       console.log("Smaller or equal");
