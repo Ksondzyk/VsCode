4f8c4e689ee4449edd238662d2f10f2263061051
Ksondzyk <roman.kson@gmail.com>
2020-03-19 18:13:50 +0200

    rom

--- javaScript/lesson4/hw3/index.js
+++ javaScript/lesson4/hw3/index.js
@@ -1,10 +1,9 @@
-let number = 1000;
 let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
   if (i % 2 === 1) {
     console.log("Found");
-    sum += i;
+    sum = i;
     if (sum * 5 > 5000) {
       console.log("Bigger");
     } else {
