5677ab30c147ca02f568ee0fd78fcfba8065a4e4
Ksondzyk <roman.kson@gmail.com>
2020-03-19 20:57:23 +0200

    result

--- javaScript/lesson4/task2/index.js
+++ javaScript/lesson4/task2/index.js
@@ -1,12 +1,25 @@
+// let m = 10;
+// let n = 20;
+// let result;
+// let sum = 1;
+
+// do {
+//   if (m % 2 === 1) {
+//     sum *= m;
+//   }
+//   m++;
+// } while (m <= n);
+
+// result += sum;
+
+// console.log("Result: ", result);
+
 let m = 10;
 let n = 20;
 let result = 1;
 
-do {
-  if (m % 2 === 1) {
-    result *= m;
-  }
-  m++;
-} while (m <= n);
-
+for (let m = 10; m <= n; n++) {
+  if (m % 2 === 1) continue;
+  result *= m;
+}
 console.log("Result: ", result);
