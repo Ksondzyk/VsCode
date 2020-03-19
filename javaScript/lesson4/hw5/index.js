309b5f14f8150d653d1008366f52b53c24034100
Ksondzyk <roman.kson@gmail.com>
2020-03-19 19:06:16 +0200

    sum

---
+++ javaScript/lesson4/hw5/index.js
@@ -0,0 +1,10 @@
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+  sum += i;
+}
+let res1 = sum / 1234;
+// console.log(res1);
+let res2 = sum % 1234;
+// console.log(res2);
+let res = res1 < res2;
+console.log(!!res);
