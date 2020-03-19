2cbc78d716685b8f4c9825c5b229fbb14b8fab0d
Ksondzyk <roman.kson@gmail.com>
2020-03-19 22:41:43 +0200

    Reload

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -1,17 +1,15 @@
 let m = 10;
 let n = 12;
-let sum = 0;
 let result = 0;
-for (let i = m; i < n && n <= 50; i++) {
-  result += i;
+for (let i = m; i <= n; i++) {
   if (i % 2 === 0 || !(i % 4 === 0)) {
-    sum += i + m + n;
+    result += i + m + n;
   }
   if (i % 3 === 0) {
-    sum -= i;
+    result -= i;
   }
   if (i % 4 === 0) {
-    sum *= i;
+    result *= i;
   }
   if (i % 5 === 0) {
     console.log(i);
