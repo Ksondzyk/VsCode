00d59f5be2e245826b06d4d856501081e24fc103
Ksondzyk <roman.kson@gmail.com>
2020-03-19 22:35:15 +0200

    Updead

--- javaScript/lesson4/hw2/index.js
+++ javaScript/lesson4/hw2/index.js
@@ -3,6 +3,7 @@ let n = 12;
 let sum = 0;
 let result = 0;
 for (let i = m; i < n && n <= 50; i++) {
+  result += i;
   if (i % 2 === 0 || !(i % 4 === 0)) {
     sum += i + m + n;
   }
@@ -15,5 +16,4 @@ for (let i = m; i < n && n <= 50; i++) {
   if (i % 5 === 0) {
     console.log(i);
   }
-  result += sum;
 }
