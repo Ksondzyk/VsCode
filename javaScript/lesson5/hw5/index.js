fc6bf2b6d39afc1a0787022a48562bf9c63a29b5
Ksondzyk <roman.kson@gmail.com>
2020-03-20 13:02:17 +0200

    result

--- javaScript/lesson5/hw5/index.js
+++ javaScript/lesson5/hw5/index.js
@@ -1,19 +1,16 @@
 function compareSums(a, b, c, d) {
   let from = 0;
   let to = 0;
-  for (let i = a; i < b; i++) {
+  let result = from > to ? true : false;
+  for (let i = a; i <= b; i++) {
     from += i;
   }
-  for (let i = c; i < d; i++) {
+  for (let i = c; i <= d; i++) {
     to += i;
   }
-  return function sum(from, to) {
-    if (from > to) {
-      return !!from;
-    } else {
-      return !!to;
-    }
+  return function(from, to) {
+    return result;
   };
 }
 
-compareSums(3, 8, 4, 7);
+compareSums(5, 8, 9, 12);
